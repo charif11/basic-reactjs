32fbb64ae589d8d585e81d74aac6f5cf7bbb9cbe
s65309010003 <s65309010003@ptniece.ac.th>
2025-02-26 22:33:10 +0700

    fix: change BrowserRouter to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter  } from "react-router-dom";
 import AppRouter from "./routes/Router";
 import "./App.css";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <AppRouter />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
