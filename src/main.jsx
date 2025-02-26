d9d63a97c9e2d65f0143705b0409163e2dc59dd9
s65309010003 <s65309010003@ptniece.ac.th>
2025-02-26 22:28:32 +0700

    fix: change BrowserRouter to HashRouter

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,12 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  // <StrictMode>
+  //   <App />
+  // </StrictMode>,
+  <React.StrictMode>
+  <HashRouter>
     <App />
-  </StrictMode>,
+  </HashRouter>
+</React.StrictMode>
 )
